adf37e43137bbf701379ef211655f107e6e66f11
Vincent Zerbib <contact@zelabstudio.com>
2021-03-19 09:53:22 +0100

    sav work

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,12 +50,13 @@ export default {
   apollo: {
     clientConfigs: {
       default: {
-        httpEndpoint:"http://localhost:3030/graphql"
+        httpEndpoint:process.env.API_URL_GRAPHQL
       }
     }
   },
   env: {
     API_URL: process.env.API_URL,
     API_SENDGRID: process.env.SENDGRID,
+    API_URL_GRAPHQL:process.env.API_URL_GRAPHQL
   }
 }
